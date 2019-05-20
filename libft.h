7cfc3dd0d48ae0987bdc07f87a379f6a60efa3dd
Kagiso MAKO <kamako@c4r9s9.wethinkcode.co.za>
2019-05-20 14:04:21 +0200

    first push

---
+++ libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamako <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 12:43:15 by kamako            #+#    #+#             */
+/*   Updated: 2019/05/20 14:03:19 by kamako           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <unistd.h>
+#include <stdlib.h>
+
+void		ft_putchar(char c);
+void		ft_putstr(char const *s);
+int			ft-strlen(char *);
+char		*ft_strcpy(char *, char *);
+char		*ft_strdup(char *);
+int			ft_memcmp(const void *, const void *, size_t);
+char		*ft_strncpy(char *, char *, int);
+char		*ft_strcat(char *, char *);
+char		*ft_strncat(char *, char *, int);
+void		*ft_memset(void *, int, size_t);
+void		ft_bzero(char *, int);
+void
+
