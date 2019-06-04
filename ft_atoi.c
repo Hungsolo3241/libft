8f9902d690a207df194c7c7f58ecbcd36f384425
Kagiso MAKO <kamako@c4r9s10.wethinkcode.co.za>
2019-06-04 15:41:52 +0200

    atoi complete

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,18 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kamako <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/03 13:44:14 by kamako            #+#    #+#             */
-/*   Updated: 2019/06/04 14:46:00 by kamako           ###   ########.fr       */
+/*   Created: 2019/06/04 15:17:07 by kamako            #+#    #+#             */
+/*   Updated: 2019/06/04 15:41:15 by kamako           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+int		ft_atoi(const char *str)
 {
+	long			res;
+	long			sign;
+	unsigned int	i;
 
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i]  == '\n' || str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - 0;
+		i++;
+	}
+	return ((int)(res * sign));
 }
