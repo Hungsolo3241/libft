aacf8d800eda9e7e130518b0a70e824825fdaa55
Kagiso MAKO <kamako@c5r10s9.wethinkcode.co.za>
2019-06-07 15:26:36 +0200

    striter, strietri

--- ft_striter.c
+++ ft_striter.c
@@ -6,10 +6,20 @@
 /*   By: kamako <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:21:21 by kamako            #+#    #+#             */
-/*   Updated: 2019/05/27 14:22:11 by kamako           ###   ########.fr       */
+/*   Updated: 2019/06/07 15:23:12 by kamako           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_striter(char *s, void (*f)(char *))
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		f(s + i);
+		i++;
+	}
+}
--- ft_striteri.c
+++ ft_striteri.c
@@ -6,10 +6,20 @@
 /*   By: kamako <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:23:01 by kamako            #+#    #+#             */
-/*   Updated: 2019/05/27 14:23:59 by kamako           ###   ########.fr       */
+/*   Updated: 2019/06/07 15:25:47 by kamako           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		f(i + s + i);
+		i++;
+	}
+}
